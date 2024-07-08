4fa26ea3531b5a0c86b9afda4c80fe78599b534e
onkarkshirsagar1234 <onkark657@gmail.com>
2024-07-08 12:36:31 +0530

    Initial commit.

---
+++ todo/src/main/java/com/todoList/ToDolistApplication.java
@@ -0,0 +1,13 @@
+package com.todoList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDolistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDolistApplication.class, args);
+	}
+
+}
